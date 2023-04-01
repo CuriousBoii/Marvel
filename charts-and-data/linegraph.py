b363cda1ea9dd5178b6e43088a12934c21fa8f3a
vishnu <tavs713@gmail.com>
2023-04-01 19:37:59 +0530

    this is final

--- charts-and-data/linegraph.py
+++ charts-and-data/linegraph.py
@@ -1,10 +1,5 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-
-
-
-
-
 data = pd.read_excel('../GovernmentSpendingPerCapita.xlsx')
 print(data.STATES)
 plt.plot(data.STATES, data.POPULATION/ 10**7, 'o')
